4b2845523eb8fd1c27f6352adfa1afca68906c07
Thomas Brus <thomas.brus@me.com>
2015-11-19 14:25:25 +0100

    Return dummy categories from mocked api

---
+++ app/mirage/fixtures/categories.js
@@ -0,0 +1,10 @@
+export default [
+  { id: 1,
+    name: 'Spelletjes & Puzzels',
+    keywords: ['spel', 'puzzel', 'nijntje', 'jumbo'],
+  },
+  { id: 2,
+    name: '...',
+    keywords: [],
+  },
+];
