f4892d2db80803110bff100155e58efca3f19be4
Thomas Brus <thomas.brus@me.com>
2015-11-29 15:30:08 +0100

    Display categories as rounded buttons

--- app/transitions.js
+++ app/transitions.js
@@ -2,14 +2,14 @@ export default function() {
   this.transition(
     this.fromRoute('categories'),
     this.toRoute('products'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.use('toUp'),
+    this.reverse('toDown')
   );
 
   this.transition(
     this.fromRoute('products'),
     this.toRoute('wishlist'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.use('toUp'),
+    this.reverse('toDown')
   );
 }
