45a8ae61ddd8c1887fc5919f7223d5c71d02626e
Thomas Brus <thomas.brus@me.com>
2015-12-02 17:17:48 +0100

    Remove unused properties from search-result model

--- app/models/search-result.js
+++ app/models/search-result.js
@@ -1,9 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  rank: DS.attr('number'),
-  resourceId: DS.attr('string'),
-  score: DS.attr('number'),
-  query: DS.attr('string'),
   product: DS.belongsTo('product', { async: false }),
 });
