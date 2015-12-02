b9b6668063d254edd77bfd83d435badf3f38e89a
Thomas Brus <thomas.brus@me.com>
2015-12-02 17:24:48 +0100

    Updated product model attributes

--- app/models/product.js
+++ app/models/product.js
@@ -1,9 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  description: DS.attr('string'),
-  imageUrl: DS.attr('string'),
-  price: DS.attr('string'),
   title: DS.attr('string'),
+  price: DS.attr('string'),
   url: DS.attr('string'),
+  imageUrl: DS.attr('string'),
 });
