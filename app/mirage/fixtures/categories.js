d1091673cc4613d5720955be141a313a776b16e7
Thomas Brus <thomas.brus@me.com>
2015-12-14 13:18:46 +0100

    Updated category icons (still a few are missing)

--- app/mirage/fixtures/categories.js
+++ app/mirage/fixtures/categories.js
@@ -7,71 +7,71 @@ export default [
   {
     id: 2,
     title: 'Bouw en Constructie',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/69/8b/698b21b7b42a98973b884936bc19e94f.png'
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/6d/4f/6d4f5983ea09622bc56e3929ee5531d9.png'
   },
   {
     id: 3,
     title: 'Buitenspeelgoed',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/0e/13/0e1385466f2aacfdfc3d85de0acd0dc4.png'
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/cb/64/cb6434057ae1eb1085cd2f22625903f2.png'
   },
   {
     id: 4,
     title: 'Creatief en Hobby',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/61/26/61269eb1048263c78c85a83649d045c6.png'
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/fd/a2/fda272b4e210fd0cc80e709d2ba64435.png'
   },
   {
     id: 5,
     title: 'Feest',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/78/46/78461211fc7ce432b6f5ee5118f5ce15.png'
+    iconUrl: 'http://emojipedia-us.s3.amazonaws.com/cache/02/e5/02e5e081415497b60674649cfd1fc12d.png'
   },
   {
     id: 6,
     title: 'Grabbelton',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/8d/ad/8dad3d087585d0e9ff1b201d318deb6a.png'
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/a0/34/a0340029fb54a0d6cbc81799c4855a39.png'
   },
   {
     id: 7,
     title: 'Leren en Experimenteren',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/1e/df/1edf5fc2c992811a5d7ec80bec9bda98.png'
+    // iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/1e/df/1edf5fc2c992811a5d7ec80bec9bda98.png'
   },
   {
     id: 8,
     title: 'Houten speelgoed',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/cb/64/cb6434057ae1eb1085cd2f22625903f2.png',
+    // iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/cb/64/cb6434057ae1eb1085cd2f22625903f2.png',
   },
   {
     id: 9,
     title: 'Kerst',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/6d/4f/6d4f5983ea09622bc56e3929ee5531d9.png',
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/48/cd/48cd033a8ee2075e6b74e531c70159d2.png',
   },
   {
     id: 10,
     title: 'Poppen en Knuffels',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/b2/05/b205cde1d140b087ca9ddd1a7b7a0593.png',
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/69/8b/698b21b7b42a98973b884936bc19e94f.png'
   },
   {
     id: 11,
     title: 'Puzzels',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/a4/06/a40602438568668fae070277a0d051f4.png',
+    // iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/a4/06/a40602438568668fae070277a0d051f4.png',
   },
   {
     id: 12,
     title: 'Spellen',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/41/bd/41bdb4e32f3d30c099899c993f2a38bc.png'
   },
   {
     id: 13,
     title: 'Rollenspel & Verkleden',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
+    // iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
   },
   {
     id: 14,
     title: 'Speelfiguren',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
+    // iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
   },
   {
     id: 15,
     title: 'Schoolspullen',
-    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/c7/21/c7213daebdd735940fb5842064280cd8.png'
   },
 ];
