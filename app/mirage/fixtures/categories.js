81080d4f132f514aaae3ee09f521a920d99b92df
Thomas Brus <thomas.brus@me.com>
2015-12-14 13:00:43 +0100

    Updated category names to match lobbes.nl

--- app/mirage/fixtures/categories.js
+++ app/mirage/fixtures/categories.js
@@ -1,74 +1,77 @@
 export default [
   {
     id: 1,
-    title: 'Auto\'s & Treinen',
-    keywords: ['auto', 'trein'],
+    title: 'Auto\'s en Voertuigen',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/d0/8e/d08ecd8632aa9e2f21808028387ab209.png'
   },
   {
     id: 2,
-    title: 'Poppen & Prinsessen',
-    keywords: ['pop', 'prinses'],
+    title: 'Bouw en Constructie',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/69/8b/698b21b7b42a98973b884936bc19e94f.png'
   },
   {
     id: 3,
-    title: 'Ridders & Piraten',
-    keywords: ['ridder', 'piraat'],
+    title: 'Buitenspeelgoed',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/0e/13/0e1385466f2aacfdfc3d85de0acd0dc4.png'
   },
   {
     id: 4,
-    title: 'Politie & Brandweer',
-    keywords: ['politie', 'brandweer'],
+    title: 'Creatief en Hobby',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/61/26/61269eb1048263c78c85a83649d045c6.png'
   },
   {
     id: 5,
-    title: 'Dieren',
-    keywords: ['hond', 'kat'],
+    title: 'Feest',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/78/46/78461211fc7ce432b6f5ee5118f5ce15.png'
   },
   {
     id: 6,
-    title: 'Superhelden',
-    keywords: ['superheld'],
+    title: 'Grabbelton',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/8d/ad/8dad3d087585d0e9ff1b201d318deb6a.png'
   },
   {
     id: 7,
-    title: 'Spelletjes & Puzzels',
-    keywords: ['spelletjes', 'puzzel'],
+    title: 'Leren en Experimenteren',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/1e/df/1edf5fc2c992811a5d7ec80bec9bda98.png'
   },
   {
     id: 8,
-    title: 'Buitenspelen',
-    keywords: ['buitenspelen'],
+    title: 'Houten speelgoed',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/cb/64/cb6434057ae1eb1085cd2f22625903f2.png',
   },
   {
     id: 9,
-    title: 'Bouwen & Constructie',
-    keywords: ['bouwen', 'constructie'],
+    title: 'Kerst',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/6d/4f/6d4f5983ea09622bc56e3929ee5531d9.png',
   },
   {
     id: 10,
-    title: 'Knutselen & Tekenen',
-    keywords: ['knutselen', 'tekenen'],
+    title: 'Poppen en Knuffels',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/b2/05/b205cde1d140b087ca9ddd1a7b7a0593.png',
   },
   {
     id: 11,
-    title: 'Glitter & Glamour',
-    keywords: ['glitter', 'glamour'],
+    title: 'Puzzels',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/a4/06/a40602438568668fae070277a0d051f4.png',
   },
   {
     id: 12,
-    title: 'Rollenspel',
-    keywords: ['rollenspel'],
+    title: 'Spellen',
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
+  },
+  {
+    id: 12,
+    title: 'Rollenspel & Verkleden',
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
+  },
+  {
+    id: 13,
+    title: 'Speelfiguren',
+    iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
+  },
+  {
+    id: 14,
+    title: 'Schoolspullen',
     iconUrl: 'https://emojipedia-us.s3.amazonaws.com/cache/f5/a1/f5a1d8d9f45704c6ff2345b024700b1b.png'
   },
 ];
