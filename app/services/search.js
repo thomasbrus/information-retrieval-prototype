8e7aee3938d9ab5ac6282c559773bb0074f1a193
Thomas Brus <thomas.brus@me.com>
2016-01-12 21:41:08 +0100

    Perform dummy query when no categories are selected

--- app/services/search.js
+++ app/services/search.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
-import DS from 'ember-data';
 
 export default Ember.Service.extend({
   store: Ember.inject.service(),
 
   perform(query) {
-    if (Ember.isBlank(query)) { return DS.PromiseArray.create({ promise: Ember.RSVP.resolve([]) }); }
-    return this.get('store').query('search-result', { q: query });
+    return this.get('store').query('search-result', { q: Ember.isBlank(query) ? '*' : query });
   }
 });
