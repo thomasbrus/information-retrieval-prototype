42059e8b6850f454857ab5a10932c3a3d8b28d55
Thomas Brus <thomas.brus@me.com>
2016-01-12 19:53:35 +0100

    Indicate price category ($, $$, $$$)

--- app/components/product-component.js
+++ app/components/product-component.js
@@ -2,13 +2,20 @@ import Ember from 'ember';
 
 let ProductComponent = Ember.Component.extend({
   classNames: ['product'],
+  classNameBindings: ['priceCategoryClass'],
 
-  productImageStyle: Ember.computed('product.imageUrl', function() {
+  imageStyle: Ember.computed('product.imageUrl', function() {
     if (this.get('product.imageUrl')) {
       return `background-image: url('${this.get('product.imageUrl')}')`.htmlSafe();
     } else {
       return null;
     }
+  }),
+
+  priceCategoryClass: Ember.computed('product.price', function() {
+    if (this.get('product.price') < 7.5) { return 'is-cheap'; }
+    if (this.get('product.price') < 22.5) { return 'is-moderate'; }
+    return 'is-expensive';
   }),
 });
 
